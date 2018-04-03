b124b027bef03182fed71bfc5af223f07360803b
linfeng <lf@aixinqianbao.com.cn>
2018-04-03 19:02:08 +0800

    RSA签名，加解密

--- src/main/java/com/springboot/signature/JdkRsaUtil.java
+++ src/main/java/com/springboot/signature/JdkRsaUtil.java
@@ -39,17 +39,66 @@ public class JdkRsaUtil {
          * 签名算法
          */
         String SIGNATURE_ALGORITHM = "MD5withRSA";
-
-        /**
-         * 获取公钥的key
-         */
-        String PUBLIC_KEY = "RSAPublicKey";
-
-        /**
-         * 获取私钥的key
-         */
-        String PRIVATE_KEY = "RSAPrivateKey";
     }
+
+    /**
+     * PKCS8密钥规范签名
+     *
+     * @param context
+     * @param privateKey
+     * @return
+     * @throws Exception
+     */
+    public static String sign(String context, String privateKey) throws Exception {
+        PrivateKey key = getPrivateKey(privateKey);
+        return sign(context.getBytes(), key);
+    }
+
+    /**
+     * 签名
+     *
+     * @param context
+     * @param privateKey
+     * @return
+     * @throws Exception
+     */
+    public static String sign(byte[] context, PrivateKey privateKey) throws Exception {
+        Signature signature = Signature.getInstance(Constants.SIGNATURE_ALGORITHM);
+        signature.initSign(privateKey);
+        signature.update(context);
+        return Base64.encode(signature.sign());
+    }
+
+    /**
+     * 签名认证
+     *
+     * @param context
+     * @param publicKey
+     * @param sign
+     * @return
+     * @throws Exception
+     */
+    public static boolean verify(String context, String publicKey, String sign) throws Exception {
+        PublicKey key = getPublicKey(publicKey);
+        return verify(context.getBytes(), key, sign);
+    }
+
+    /**
+     * 签名认证
+     *
+     * @param context
+     * @param publicKey
+     * @param sign
+     * @return
+     * @throws Exception
+     */
+    public static boolean verify(byte[] context, PublicKey publicKey, String sign) throws Exception {
+        Signature signature = Signature.getInstance(Constants.SIGNATURE_ALGORITHM);
+        signature.initVerify(publicKey);
+        signature.update(context);
+        return signature.verify(Base64.decode(sign));
+    }
+
 
     /**
      * 使用PKCS8私钥规范加密
@@ -59,10 +108,7 @@ public class JdkRsaUtil {
      * @return
      */
     public static String encryptByPrivateKey(String context, String privateKey) throws Exception {
-        byte[] keyBytes = Base64.decode(privateKey.getBytes());
-        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
-        KeyFactory keyFactory = KeyFactory.getInstance(Constants.KEY_ALGORITHM);
-        PrivateKey key = keyFactory.generatePrivate(keySpec);
+        PrivateKey key = getPrivateKey(privateKey);
         return Base64.encode(encryptByPrivateKey(context.getBytes(), key));
     }
 
@@ -74,10 +120,7 @@ public class JdkRsaUtil {
      * @return
      */
     public static String decryptByPublicKey(String context, String publicKey) throws Exception {
-        byte[] keyBytes = Base64.decode(publicKey.getBytes());
-        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
-        KeyFactory keyFactory = KeyFactory.getInstance(Constants.KEY_ALGORITHM);
-        PublicKey key = keyFactory.generatePublic(keySpec);
+        PublicKey key = getPublicKey(publicKey);
         return new String(decryptByPublicKey(Base64.decode(context), key));
     }
 
@@ -89,10 +132,7 @@ public class JdkRsaUtil {
      * @return
      */
     public static String decryptByPrivateKey(String context, String privateKey) throws Exception {
-        byte[] keyBytes = Base64.decode(privateKey.getBytes());
-        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
-        KeyFactory keyFactory = KeyFactory.getInstance(Constants.KEY_ALGORITHM);
-        PrivateKey key = keyFactory.generatePrivate(keySpec);
+        PrivateKey key = getPrivateKey(privateKey);
         return new String(decryptByPrivateKey(Base64.decode(context), key));
     }
 
@@ -104,10 +144,7 @@ public class JdkRsaUtil {
      * @return
      */
     public static String encryptByPublicKey(String context, String publicKey) throws Exception {
-        byte[] keyBytes = Base64.decode(publicKey.getBytes());
-        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
-        KeyFactory keyFactory = KeyFactory.getInstance(Constants.KEY_ALGORITHM);
-        PublicKey key = keyFactory.generatePublic(keySpec);
+        PublicKey key = getPublicKey(publicKey);
         return Base64.encode(encryptByPublicKey(context.getBytes(), key));
     }
 
@@ -198,12 +235,40 @@ public class JdkRsaUtil {
         return data;
     }
 
-    public static void main(String[] args) throws Exception {
+    /**
+     * 获取X509Encoded规范的公钥
+     * @param publicKey
+     * @return
+     * @throws Exception
+     */
+    private static PublicKey getPublicKey(String publicKey) throws Exception{
+        byte[] keyBytes = Base64.decode(publicKey.getBytes());
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(Constants.KEY_ALGORITHM);
+        PublicKey key = keyFactory.generatePublic(keySpec);
+        return key;
+    }
 
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(Constants.KEY_ALGORITHM);
-        keyPairGenerator.initialize(1024);
-        KeyPair key = keyPairGenerator.generateKeyPair();
+    /**
+     * 获取PKCS8规范的私钥
+     * @param privateKey
+     * @return
+     * @throws Exception
+     */
+    private static PrivateKey getPrivateKey(String privateKey) throws Exception{
+        byte[] keyBytes = Base64.decode(privateKey.getBytes());
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(Constants.KEY_ALGORITHM);
+        PrivateKey key = keyFactory.generatePrivate(keySpec);
+        return key;
+    }
 
+//    public static void main(String[] args) throws Exception {
+//
+//        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(Constants.KEY_ALGORITHM);
+//        keyPairGenerator.initialize(1024);
+//        KeyPair key = keyPairGenerator.generateKeyPair();
+//
 //        //私钥加密
 //        String encryptedStr = encryptByPrivateKey("测试", Base64.encode(key.getPrivate().getEncoded()));
 //        System.out.println("加密后：" + encryptedStr);
@@ -211,14 +276,22 @@ public class JdkRsaUtil {
 //        //公钥解密
 //        String decryptedStr = decryptByPublicKey(encryptedStr, Base64.encode(key.getPublic().getEncoded()));
 //        System.out.println("解密后：" + decryptedStr);
-
-
+//
+//
 //        //公钥加密
-//        String encryptedStr = encryptByPublicKey("测试", Base64.encode(key.getPublic().getEncoded()));
-//        System.out.println("加密后：" + encryptedStr);
+//        String encryptedStr1 = encryptByPublicKey("测试", Base64.encode(key.getPublic().getEncoded()));
+//        System.out.println("加密后：" + encryptedStr1);
 //
 //        //私钥解密
-//        String decryptedStr = decryptByPrivateKey(encryptedStr, Base64.encode(key.getPrivate().getEncoded()));
-//        System.out.println("解密后：" + decryptedStr);
-    }
+//        String decryptedStr1 = decryptByPrivateKey(encryptedStr1, Base64.encode(key.getPrivate().getEncoded()));
+//        System.out.println("解密后：" + decryptedStr1);
+//
+//        //私钥签名
+//        String sign = sign("测试", Base64.encode(key.getPrivate().getEncoded()));
+//        System.out.println("签名：" + sign);
+//
+//        //公钥认证
+//        boolean verify = verify("测试", Base64.encode(key.getPublic().getEncoded()),sign);
+//        System.out.println("签名认证：" + verify);
+//    }
 }
